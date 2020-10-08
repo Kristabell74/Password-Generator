4cb0325149a42c15b160acf131e84b1618cf2f06
Kristabell Johnson <bell77iam@gmail.com>
2020-10-08 00:59:51 -0600

    worked on making arrays for javascript. Started some alerts and functions

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Four arrays with the numbers, letters and symbols that the computer will choose from
+var randomLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var randomUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var randomSymbol = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '[', ']', '{', '}', '+', '=', '~'];
+var randomNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+if ()
+
+  //Concatenates (merges) randomLower with randomUpper, randomSymbol, randomNumber
+  var aPassword = randomLower.concat(randomUpper, randomSymbol, randomNumber);
+
+//Alerts 
+if ("click") alert("Choose Characteristics of Password.")
+
+
+
+
+//Funtions
+
+function getRandomLower() {
+
+}
+
+function getRandomUpper() {
+
+}
+
+function getRandomSymbol() {
+
+}
+function getRandomNumber() {
+
+}
+
+
+
+console.log()
