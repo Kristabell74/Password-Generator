2db2259ed1e613794bb4325a065c188b92b96b66
Kristabell Johnson <bell77iam@gmail.com>
2020-10-11 00:18:03 -0600

    added prompts and alerts if else statements

--- script.js
+++ script.js
@@ -6,32 +6,70 @@ var randomUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 var randomSymbol = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '[', ']', '{', '}', '+', '=', '~'];
 var randomNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+//Choose random Items from the 4 arrays that have been created
+var randomCharacters = aPassword[Math.floor(Math.random().length = "8-128 characters")];
+//Alerts 
+
+var characterNumber = confirm("How many Characters would you like your password to contain?");
+for (var i = 0; <6; i++) {
+  randomPrompt = 0++
+
+}
+//Prompts and Alerts
+var randomLower = confirm("Would you like to use Lower case numbers in your password?");
+console.log(True == False)
+
+var randomUpper = confirm("Would you like to use Upper case letters in your password?");
+console.log(True == False)
+
+var randomSymbol = confirm("Would you like to use Symbols in your password?");
+console.log(True == False)
+
+var randomNumber = confirm("Would you like to use numbers in your password?")
+console.log(True == False)
+
+//Control Flow
+
+if (confirmRandomLower) {
+  alert("Ok Lower Case letters added")
+}
+else {
+  alert("No Lower Case Letters added")
+}
+
+
+if (confirmRandomUpper) {
+  alert("Ok Upper Case letters added")
+}
+else {
+  alert("No Upper Case Letters added")
+}
+
+if (confirmRandomSymbol) {
+  alert("Ok Symbols added")
+}
+else {
+  alert("No Symbols added")
+}
+
+if (confirmRandomNumber) {
+  alert("Ok Numbers added")
+}
+else {
+  alert("No Numbers added")
+}
+
+
 
 //Code for the computer to choose random character
+
 var sm1 = [randomLower, randomUpper, randomSymbol, randomNumber];
-for (var i = 0; i < 24; i++) {
-  randomPrompt = Math.floor((Math.random() * 12));
+for (var i = 0; i < 8; i++) {
+  randomPrompt = Math.floor((Math.random() * 0++));
   sm1.push(window["random" + i][randomPrompt])
 }
 
 console.log(sm1)
-
-//Choose random Items from the 4 arrays that have been created
-var randomItems = aPassword[Math.floor(Math.random().length = "12 characters")];
-
-
-
-//Alerts 
-
-alert("How many Characters would you like your password to contain?");
-
-alert("Would you like to use Lower case numbers in your password?");
-
-alert("Would you like to use Upper case letters in your password?");
-
-alert("Would you like to use Symbols in your password?");
-
-alert("Would you like to use numbers in your password?")
 
 
 
